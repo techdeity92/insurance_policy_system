62395bad1734d1bbdd5b2e91669c93837e620713
tech_deity <158518636+techdeity92@users.noreply.github.com>
2025-05-26 17:46:29 -0500

    Add files via upload

--- product.py
+++ product.py
@@ -1,7 +1,16 @@
-{
-	"folders": [
-		{
-			"path": "."
-		}
-	]
-}
+class Product:
+    def __init__(self, product_id, name, price):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.status = "available"
+
+    def update(self, new_name, new_price):
+        self.name = new_name
+        self.price = new_price
+
+    def suspend(self):
+        self.status = "suspended"
+
+    def display_product(self):
+        print(f"Product ID: {self.product_id}, Name: {self.name}, Price: ${self.price}, Status: {self.status}")
